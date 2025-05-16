0b9ed01268c4efd98d2ca0c0fe095ef128575534
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-05-16 16:40:04 -0700

    set up 1password git commit signing

--- modules/home.nix
+++ modules/home.nix
@@ -26,6 +26,20 @@
       nix-direnv.enable = true;
     };
 
+    programs.git = {
+      enable = true;
+
+      userEmail = "jesse@jbhannah.net";
+      userName = "Jesse Brooklyn Hannah";
+
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOAnSncawa7Y3U7/ZUkqnXLrAgJ5mxNLLKOgM20+dsV+";
+        format = "ssh";
+        signByDefault = true;
+        signer = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+    };
+
     programs.ssh = {
       enable = true;
       matchBlocks."*".identityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
