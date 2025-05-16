77038e389e17b0806490ca7e1ac730b8fae459f3
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-05-16 12:01:45 -0700

    add homebrew path and casks

--- flake.nix
+++ flake.nix
@@ -10,11 +10,28 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
+      nix.enable = false;
+
+      environment.extraInit = ''
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      '';
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = [];
 
-      nix.enable = false;
+      homebrew.enable = true;
+      homebrew.caskArgs.appdir = "~/Applications";
+      homebrew.casks = [
+        {
+          name = "1password";
+          args.appdir = "/Applications";
+        }
+        "1password-cli"
+        "arc"
+        "visual-studio-code"
+        "warp"
+      ];
 
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
