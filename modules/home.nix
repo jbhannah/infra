026aaf4a40b12647efa82f902891e6fab0bb5093
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-05-16 14:57:51 -0700

    use 1password ssh agent

--- modules/home.nix
+++ modules/home.nix
@@ -15,5 +15,10 @@
       enable = true;
       nix-direnv.enable = true;
     };
+
+    programs.ssh = {
+      enable = true;
+      matchBlocks."*".identityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+    };
   };
 }
