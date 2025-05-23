f3ae5744dfbc1183e04a9b7dcfe5dfe8375794de
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-05-23 07:49:59 -0700

    disable copySystemConfiguration
    not supported with flakes

--- hosts/nixos/tinkaton/default.nix
+++ hosts/nixos/tinkaton/default.nix
@@ -138,11 +138,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
   system.autoUpgrade = {
     enable = true;
     persistent = true;
