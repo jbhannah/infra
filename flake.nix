60b4399b9b2c02599418caeae80bfe7d333bd72a
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-08-22 12:47:08 -0700

    switch to nixpkgs-unstable branch

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Infrastructure flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
